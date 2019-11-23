74088742d6f82f31c9483af4497eb750cc1782f6
Sebastian Kozłowicz <sebastian.kozlowicz@hotmail.com>
2019-11-23 17:47:50 +0100

    Add generating txt file function call and bind to button click

--- moduls/index.js
+++ moduls/index.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
         $('#results').val(converter.getBinaryNumbers());
         $('#timeElapsed').text(timer.elapsedTime + 's');
     });
+
+    $('#generateTxtFile').on('click', function() {
+        let converter = new Converter();
+        let timer = new Timer();
+        let streamWriter = new StreamWriter();
+
+        timer.start();
+        converter.getNDigitBinaryNumbers($('#bitsNumber').val());
+        timer.stop();
+
+        streamWriter.save(converter.getBinaryNumbers(), timer.elapsedTime);
+    });
 });
